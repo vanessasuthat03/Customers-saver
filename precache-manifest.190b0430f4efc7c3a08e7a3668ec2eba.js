d702ba3ee354decbbb37fa550c79938ce9ec929f
Vanessa Suthat <vanessasuthat@vanessas-mbp-2.mynet>
2020-09-29 11:57:25 +0200

    Updates

---
+++ precache-manifest.190b0430f4efc7c3a08e7a3668ec2eba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e9312d25210dd6fd3c457656280852",
+    "url": "/Customers-saver/index.html"
+  },
+  {
+    "revision": "fec603f671c52db39fd4",
+    "url": "/Customers-saver/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be38f20ecb07d8fce3c8",
+    "url": "/Customers-saver/static/js/2.8cf2ef07.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Customers-saver/static/js/2.8cf2ef07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec603f671c52db39fd4",
+    "url": "/Customers-saver/static/js/main.d48d5fe0.chunk.js"
+  },
+  {
+    "revision": "776cbd8e9e2584c6deee",
+    "url": "/Customers-saver/static/js/runtime-main.d39c390a.js"
+  },
+  {
+    "revision": "0a879f77402ce9a41df6fc3055fe8519",
+    "url": "/Customers-saver/static/media/headerimage.0a879f77.png"
+  }
+]);
